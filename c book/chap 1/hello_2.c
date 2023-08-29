ec40320123812d2060534afe83141d01b92804f7
nnnolan <pestanonolan@gmail.com>
2023-08-28 22:27:31 -0500

    beep boop

--- c book/chap 1/hello_2.c
+++ c book/chap 1/hello_2.c
@@ -3,6 +3,7 @@
 int main(){
 
     printf("hello, world\n");
-
+    printf("this is a test, using the backslash c, \c");
+    // note that the backslash c does not work in the terminal, \c isn't recognized because it's not a valid escape sequence
     return 0;
 }
