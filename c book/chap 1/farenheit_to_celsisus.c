b1c39231e103c082a36e40c8b5c93f9e4afafb68
nnnolan <pestanonolan@gmail.com>
2023-08-31 23:06:02 -0500

    Update farenheit_to_celsisus.c

--- c book/chap 1/farenheit_to_celsisus.c
+++ c book/chap 1/farenheit_to_celsisus.c
@@ -28,8 +28,28 @@ int main() {
 
     /* this time with floats */
 
-    // float fahr2, cel2;
-    // int upper2, lower2, step2;
+    float fahr2, cel2;
+    int upper2, lower2, step2;
+
+    lower2 = 0;
+    upper2 = 300;
+    step2 = 20;
+
+    fahr = lower;
+    while (fahr<=upper) {
+
+
+        /* note how we don't have to multiply by 5/9 because we're using floats, andfloats can have decimal values */
+        cel2 = (5.0/9.0) * (fahr2-32.0);
+
+        /* 
+        the 3.0f\t%6.1f is a format specifier, it's a placeholder for a float value, the first %3.0f is replaced by the value of fahr2,
+         the second %6.1f is replaced by the value of cel2
+         % tells format specifier, 3 gives the minimum width, and .0 gives the number of decimal places, zero in this case, f is the type of value, float, \t is a tab character, it's a whitespace character that moves the cursor to the next tab stop... sick
+        */
+        printf("%3.0f\t%6.1f\n", fahr2, cel2);
+
+    }
 
 
 
