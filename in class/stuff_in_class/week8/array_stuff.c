31ddb5880668e58f06aecac5c28607e7369d0d5b
nnnolan <pestanonolan@gmail.com>
2023-10-12 10:44:49 -0500

    gauss

--- in class/stuff_in_class/week8/array_stuff.c
+++ in class/stuff_in_class/week8/array_stuff.c
@@ -3,18 +3,38 @@
 
 int main() {
 
-    int x[5] = {1, 2, 3, 4, 5};// creates  array
+    // int x[5] = {1, 2, 3, 4, 5};// creates  array
 
 
-    printf("x[4] = %d\n", x[4]);
+    // printf("x[4] = %d\n", x[4]);
 
-    for (int i=0; i<100; i++) {
+    // for (int i=0; i<100; i++) {
 
 
-        printf("x[%d]: %d\n", i, x[i]);  
+    //     printf("x[%d]: %d\n", i, x[i]);  
     
-    }
+    // }
 
+    // gaussian math
+    int x[100] = {0}; // sets array to 100 0's
+
+    int lower, upper = 0;
+    scanf("%d", &lower);
+    scanf("%d", &upper);
+
+    for (int i=lower, index =0; i<=upper; i++, index++) {
+
+        x[index] = lower;
+
+
+    }
+    int sum = 0;
+    for(int i=0; i<100; i++){
+
+        printf("%d\n", x[i]);
+        sum +=x[i];
+    }
+    printf("sum = %d", sum);
     return 0;
 
 }
