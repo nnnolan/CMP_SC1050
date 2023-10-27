3b3b4d0badc02b23d70507ff5cc44f4fddebefaf
nnnolan <pestanonolan@gmail.com>
2023-10-27 09:02:30 -0500

    hard problem in c
    i did get the problem wrong

---
+++ in class/stuff_in_class/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int x = 1; 
+    
+        {
+
+            int x =2;
+
+            {
+                int x=3;
+            }
+
+            printf("%d", x);
+        }
+
+
+
+}
